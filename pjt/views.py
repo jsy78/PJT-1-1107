72c00b52776e1d2c16fd83cec9d86f4962cb1520
min <smh92333@gmail.com>
2022-11-01 23:52:21 +0900

    main.html 배너, 리스트 추가

--- pjt/views.py
+++ pjt/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from articles.models import Article
 
 def main(request):
-    return render(request, "main.html")
+    articles = Article.objects.order_by("-pk")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "main.html", context)
