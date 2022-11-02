45a9e1cefaefb549cb52497f1fef5f1a25601c2a
jsy78 <whtjd1541@gmail.com>
2022-11-02 09:43:22 +0900

    accounts/features template base

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,22 +13,38 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = (
             "username",
+            "nickname",
+            "profile",
             "first_name",
             "last_name",
             "email",
         )
+        labels = {
+            "username": "아이디",
+            "nickname": "닉네임",
+            "profile": "프로필 이미지",
+            "first_name": "이름",
+            "last_name": "성",
+            "email": "이메일",
+        }
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        if len(get_user_model().objects.filter(username=username)):
+            raise ValidationError("중복된 아이디가 있습니다.")
+        return username
+
+    def clean_nickname(self):
+        nickname = self.cleaned_data["nickname"]
+        if len(get_user_model().objects.filter(nickname=nickname)):
+            raise ValidationError("중복된 닉네임이 있습니다.")
+        return nickname
 
     def clean_email(self):
         email = self.cleaned_data["email"]
         if len(get_user_model().objects.filter(email=email)):
             raise ValidationError("중복된 이메일이 있습니다.")
         return email
-
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        if len(get_user_model().objects.filter(username=username)):
-            raise ValidationError("중복된 이름이 있습니다.")
-        return username
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -39,19 +55,28 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = (
+            "nickname",
+            "profile",
             "first_name",
             "last_name",
             "email",
         )
+        labels = {
+            "nickname": "닉네임",
+            "profile": "프로필 이미지",
+            "first_name": "이름",
+            "last_name": "성",
+            "email": "이메일",
+        }
+
+    def clean_nickname(self):
+        nickname = self.cleaned_data["nickname"]
+        if len(get_user_model().objects.filter(nickname=nickname)) >= 2:
+            raise ValidationError("중복된 닉네임이 있습니다.")
+        return nickname
 
     def clean_email(self):
         email = self.cleaned_data["email"]
-        if len(get_user_model().objects.filter(email=email)):
+        if len(get_user_model().objects.filter(email=email)) >= 2:
             raise ValidationError("중복된 이메일이 있습니다.")
         return email
-
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        if len(get_user_model().objects.filter(username=username)):
-            raise ValidationError("중복된 이름이 있습니다.")
-        return username
