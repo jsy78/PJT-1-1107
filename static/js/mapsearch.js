81e16a9baf754f5541781a3eaff846c2654002f6
kangdh208 <kangdh208@gmail.com>
2022-11-04 20:58:04 +0900

    map location finding

--- static/js/mapsearch.js
+++ static/js/mapsearch.js
@@ -11,9 +11,9 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 var geocoder = new kakao.maps.services.Geocoder();
 
 // 주소 받아오기
-// var address = document.getElementById('detailLocation').value;
+var address = document.getElementById('detailLocation').value;
 // 주소로 좌표를 검색합니다
-geocoder.addressSearch(address, function (result, status) {
+geocoder.addressSearch(`"${address}"`, function (result, status) {
 
     // 정상적으로 검색이 완료됐으면 
     if (status === kakao.maps.services.Status.OK) {
