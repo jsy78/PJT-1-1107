4f19379857118c84a778d0e0ea6d47fa866813f1
Sunjoo IM <tjss1580@gmail.com>
2022-11-04 16:47:09 +0900

    마이페이지

--- articles/admin.py
+++ articles/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Article, Comment
 
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'user', 'created_at', 'updated_at')
 
 class CommentAdmin(admin.ModelAdmin):
     pass
