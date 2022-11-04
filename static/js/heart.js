52f23787ac904d77addb6e152997b480d5af3caf
kangdh208 <kangdh208@gmail.com>
2022-11-04 14:26:31 +0900

    javascript file seperate

--- static/js/heart.js
+++ static/js/heart.js
@@ -1,26 +1,28 @@
-// (1) 좋아요 버튼
 const likeBtn = document.querySelector('#like-btn')
-// (2) 좋아요 버튼을 클릭하면, 함수 실행
+
 likeBtn.addEventListener('click', function (event) {
-    // 서버로 비동기 요청을 하고 싶음
-    console.log(event.target.dataset)
+    const articleId = event.currentTarget.dataset.articleId
     axios({
-        method: 'get',
-        url: `/articles/${event.target.dataset.articleId}/like/`
+        method: "GET",
+        url: `/articles/${articleId}/likes/`
     })
         .then(response => {
-            console.log(response)
-            console.log(response.data)
-            // event.target.classList.toggle('bi-heart')
-            // event.target.classList.toggle('bi-heart-fill')
-            if (response.data.isLiked === true) {
-                event.target.classList.add('bi-heart-fill')
-                event.target.classList.remove('bi-heart')
-            } else {
-                event.target.classList.add('bi-heart')
-                event.target.classList.remove('bi-heart-fill')
+            if (response.data.is_liked) {
+                const heartIcon = document.querySelector('#heart-icon')
+                const likeCount = document.querySelector('#like-count')
+                heartIcon.classList.add('bi-heart-fill')
+                heartIcon.classList.remove('bi-heart')
+                likeCount.innerText = response.data.like_count
             }
-            const likeCount = document.querySelector('#like-count')
-            likeCount.innerText = response.data.likeCount
+            else {
+                const heartIcon = document.querySelector('#heart-icon')
+                const likeCount = document.querySelector('#like-count')
+                heartIcon.classList.add('bi-heart')
+                heartIcon.classList.remove('bi-heart-fill')
+                likeCount.innerText = response.data.like_count
+            }
+        })
+        .catch(error => {
+            console.log(error.response)
         })
 })
